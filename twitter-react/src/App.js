65c6ee1599ec3645ebd29b59eb535b513f91a9cd
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2023-03-30 01:55:23 -0400

    Routers Added

--- twitter-react/src/App.js
+++ twitter-react/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
+
+import Home from './Components/Dashboard/Home'
 import Login from './Components/LogIn/LogIn';
 import NewUser from './Components/NewUser/new';
 
@@ -17,9 +19,14 @@ const App=()=>{
 
 	return(
 		<div>
+			<BrowserRouter>
+				<Switch>
+			< Route path ='/' component={Home}/>
 			{
-				form == Login ? <Login formSwitch={toggleForm}/> : <NewUser/>
+				form === Login ? <Login formSwitch={toggleForm}/> : <NewUser/>
 			}
+				</Switch>
+			</BrowserRouter>
 		</div>
 	)
 }
