ee44ff23f482e4a72c266722ba811593167f4035
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2023-03-30 23:35:39 -0400

    CRUD operations updated

--- twitter-react/src/Components/CRUD/Post.js
+++ twitter-react/src/Components/CRUD/Post.js
@@ -1,9 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-export const Post = () => {
-    return(
-        <div>
+function TodoList() {
+    const [data, setData] = useState([]);
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [id, setId] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // FETCH METHOD
+    const fetchData = async () => {
+        const res = await fetch("enter URL here");
+        const result = await res.json();
+        setData(result)
+    }
+
+    // ADD METHOD
+    const addData = async () => {
+        const response = await fetch("URL here", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                age,
+            })
+        })
+        fetchData();
+    }
+
+        // UPDATE METHOD
+    const updateData = async () => {
+        const response = await fetch("URL HERE/${id}", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                age,
+            }),
+        });
+        fetchData();
+    };
+
+        // DELETE METHOD
+    const deleteData = async (id) => {
+        const response = await fetch("URL HERE/${id}", {
+            method: "DELETE",
+        });
+        fetchData();
+    };
+
+    
+  return (
+    <div>
+        <h1>Test</h1>
+        <form>
+            <input
+                type='text'
+                placeholder='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+
+            <input
+                type='number'
+                placeholder='Age'
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+            />
             
-        </div>
-    )
+            <button type='button' onClick={addData}>
+                Add
+            </button>
+            <button type='button' onClick={updateData}>
+                Update
+            </button>
+        </form>
+        <ul>
+            {data.map((item) => (
+                <li key={item.id}>
+                    {item.name} - {item.age}
+                    <button type='button' onClick={() => deleteData(item.id)}>
+                        Delete
+                    </button>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
 }
+
+export default TodoList
